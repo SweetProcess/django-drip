068c3ead5544c7dbf82ccda60d4fff487472be06
Jervis Whitley <jtrain@users.noreply.github.com>
2021-12-20 04:17:28 +0000

    update settings file to comply with django 3+

--- testsettings.py
+++ testsettings.py
@@ -1,61 +1,61 @@
-SECRET_KEY = 'dripdripdripdripdripdripdrip'
+SECRET_KEY = "dripdripdripdripdripdripdrip"
 
 # MUST SPECIFY TO MAKE USE OF DJANGO DRIP
-DRIP_FROM_EMAIL = ''
+DRIP_FROM_EMAIL = ""
 DEBUG = True
 
-SECRET_KEY = 'whatever/you/want-goes-here'
+SECRET_KEY = "whatever/you/want-goes-here"
 
-SECRET_KEY="whatever"
+SECRET_KEY = "whatever"
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'sqlite.db',
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": "sqlite.db",
     },
 }
 
 INSTALLED_APPS = (
-    'django.contrib.auth',
-    'django.contrib.admin',
-    'django.contrib.sessions',
-    'django.contrib.contenttypes',
-    'django.contrib.staticfiles',
-
-    'drip',
-
+    "django.contrib.auth",
+    "django.contrib.admin",
+    "django.contrib.sessions",
+    "django.contrib.contenttypes",
+    "django.contrib.staticfiles",
+    "django.contrib.messages",
+    "drip",
     # testing only
-    'credits',
+    "credits",
 )
 
 MIDDLEWARE = (
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
 )
 
 
-
 USE_TZ = True
-TIME_ZONE = 'UTC'
+TIME_ZONE = "UTC"
 
-AUTH_PROFILE_MODULE = 'credits.Profile'
+AUTH_PROFILE_MODULE = "credits.Profile"
 
-ROOT_URLCONF = 'test_urls'
+ROOT_URLCONF = "test_urls"
 
-STATIC_URL = '/static/'
+STATIC_URL = "/static/"
 STATICFILES_DIRS = ()
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': ['django.contrib.auth.context_processors.auth']
-        }
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
+            ]
+        },
     },
 ]
-
