04084b34130882fdae586c2cb036d4d739e97b0f
Adam Jakubowski <ajakubo1@gmail.com>
2024-10-08 13:37:46 +0200

    style

--- credits/models.py
+++ credits/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 class Account(models.Model):
     owner = models.ForeignKey(
-        'auth.User', related_name='accounts', on_delete=models.CASCADE
+        "auth.User", related_name="accounts", on_delete=models.CASCADE
     )
 
 
@@ -12,8 +12,9 @@ class Profile(models.Model):
     """
     For testing, track the number of "credits".
     """
+
     user = models.OneToOneField(
-        'auth.User', related_name='profile', on_delete=models.CASCADE
+        "auth.User", related_name="profile", on_delete=models.CASCADE
     )
     credits = models.PositiveIntegerField(default=0)
     account = models.ForeignKey(Account, null=True, on_delete=models.CASCADE)
@@ -22,4 +23,6 @@ class Profile(models.Model):
 def user_post_save(sender, instance, created, raw, **kwargs):
     if created:
         Profile.objects.create(user=instance)
+
+
 models.signals.post_save.connect(user_post_save, sender=User)
--- test_urls.py
+++ test_urls.py
@@ -1,7 +1,6 @@
 from django.urls import re_path
 from django.contrib import admin
+
 admin.autodiscover()
 
-urlpatterns = [
-    re_path(r'^admin/', admin.site.urls)
-]
+urlpatterns = [re_path(r"^admin/", admin.site.urls)]
